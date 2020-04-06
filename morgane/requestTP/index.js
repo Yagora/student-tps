aa7f0622e41417af2e9a27ea97ac88b76c0de5b8
Arlune <morgane.gaillard69@gmail.com>
2020-04-06 10:12:48 +0200

    add variable for person name

--- morgane/requestTP/index.js
+++ morgane/requestTP/index.js
@@ -1,24 +1,8 @@
 "use strict";
 
 const request = require("request");
-/* 
-request("https://jsonplaceholder.typicode.com/users", function (__, ___, body) {
-  let users = JSON.parse(body);
 
-  found = users.find((person) => person.name === "Ervin Howell");
-  console.log(found);
-
-  request("https://jsonplaceholder.typicode.com/posts", function (
-    __,
-    ___,
-    body
-  ) {
-    let posts = JSON.parse(body);
-
-    const filteredPosts = posts.filter((post) => post.userId === found.id);
-    console.log(filteredPosts);
-  });
-}); */
+const User = "Ervin Howell";
 
 const getFilteredPost = new Promise(function (resolve, reject) {
   request("https://jsonplaceholder.typicode.com/users", function (
@@ -28,7 +12,7 @@ const getFilteredPost = new Promise(function (resolve, reject) {
   ) {
     let users = JSON.parse(body);
 
-    let found = users.find((person) => person.name === "Ervin Howell");
+    let found = users.find((person) => person.name === User);
 
     resolve(found);
   });
